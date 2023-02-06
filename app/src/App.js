e86ad6b5da9bfd53f535557c9d7441f58cdbe86a
João Vitor Lessa <joao.barros@fatec.sp.gov.br>
2023-02-06 13:11:44 -0300

    Horizontal Parallax Example 1

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Cat from './pages/Cat'
+import HorizontalSections from './pages/HorizontalSections'
 import './App.css'
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path='/cat' element={<Cat/>} />
+      <Route path='/cat' element={<Cat />} />
+      <Route path='/horizontal-sections' element={<HorizontalSections/>} />
     </Routes>
   )
 }
---
+++ app/src/pages/HorizontalSections.jsx
@@ -0,0 +1,73 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { useRef } from 'react'
+
+const IndividualPage = ({ offset, gradient, onClick }) => {
+    return (
+        <>
+            <ParallaxLayer offset={offset} speed={0.2} onClick={onClick}>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        backgroundColor: '#20232f',
+                        
+                        clipPath: 'polygon(20% 0, 70% 0, 50% 100%, 0% 100%)',
+                        position: 'absolute',
+                        width: '170%',
+                        height: '100%',
+                        cursor: 'pointer'
+                    }}
+                />
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={offset} speed={0.6} onClick={onClick}>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        clipPath: 'polygon(70% 0, 100% 0, 80% 100%, 50% 100%)',
+                        position: 'absolute',
+                        widht: '170%',
+                        height: '100%',
+                        cursor: 'pointer',
+                        background: 'linear-gradient(to right, deeppink 0%, coral 100%)'
+                    }}
+                />
+            </ParallaxLayer>
+
+            <ParallaxLayer
+                offset={offset}
+                speed={0.3}
+                style={{
+                    fontSize: '300px',
+                    color: '#545864',
+                    justifyContent: 'start'
+                }}
+            >
+                <span>0{offset + 1}</span>
+            </ParallaxLayer>
+        </>
+    )
+}
+
+const HorizontalSections = () => {
+    const ref = useRef()
+
+    return (
+        <div>
+            <Parallax
+                ref={ref}
+                pages={3}
+                horizontal
+            >
+                <IndividualPage offset={0} gradient="pink" onClick={() => ref.current.scrollTo(1)} />
+                <IndividualPage offset={1} gradient="teal" onClick={() => ref.current.scrollTo(2)} />
+                <IndividualPage offset={2} gradient="tomato" onClick={() => ref.current.scrollTo(0)} />
+            </Parallax>
+        </div>
+    )
+}
+
+export default HorizontalSections
